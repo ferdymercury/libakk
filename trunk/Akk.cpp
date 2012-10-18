388a285b97e6b2494cd5c4d5ec44222c45aa5e6c
mnagl <mnagl@51ce2ffc-923b-43e4-8068-8943c8107e06>
2012-10-18 10:06:44 +0000

    Finished transformation to a shared library Added qmake project file

--- trunk/Akk.cpp
+++ trunk/Akk.cpp
@@ -1,157 +1,191 @@
 //
-//  AkkCalc.cpp
-//  AkkCalc
-//
 //  Copyright 2012 CERN, João Guilherme Martins Correia & Marcelo Baptista Barbosa.
 //  All rights reserved.
 //
 
+#include "Akk.h"
 #include <cmath>
-#include "AkkCalc.h"
+#include <gsl/gsl_sf_coupling.h>
+
+class AkkPrivate {
+
+public:
+    bool valid;
+
+    // Input variables
+    int dbl_ji; // initial state spin
+    int dbl_j; // intermediate state spin
+    int dbl_jf; // final state spin
+    double d1;
+    double d2;
+    double firstAl;
+    double firstAl1;
+    double firstB211;
+    double firstB212;
+    double firstB222;
+    double firstB411;
+    double firstB412;
+    double firstB422;
+    double secondAl;
+    double secondAl1;
+    double secondB211;
+    double secondB212;
+    double secondB222;
+    double secondB411;
+    double secondB412;
+    double secondB422;
+    // Output variables
+    double a22;
+    double a24;
+    double a42;
+    double a44;
+
+    AkkPrivate()
+        : valid(false),
+          dbl_ji(0),
+          dbl_j(0),
+          dbl_jf(0),
+          d1(0.0),
+          d2(0.0),
+          firstAl(1.0),
+          firstAl1(1.0),
+          firstB211(1.0),
+          firstB212(1.0),
+          firstB222(1.0),
+          firstB411(1.0),
+          firstB412(1.0),
+          firstB422(1.0),
+          secondAl(1.0),
+          secondAl1(1.0),
+          secondB211(1.0),
+          secondB212(1.0),
+          secondB222(1.0),
+          secondB411(1.0),
+          secondB412(1.0),
+          secondB422(1.0),
+          a22(0.0),
+          a24(0.0),
+          a42(0.0),
+          a44(0.0)
+    {
+    }
+
+    void calculateAkk()
+    {
+        //  A2 (gamma 1)
+        double a2gamma1 = calculateComponent(2, dbl_ji, dbl_j, firstAl1, firstAl, firstB211, firstB212, firstB222, d1);
+
+        //  A4 (gamma 1)
+        double a4gamma1 = calculateComponent(4, dbl_ji, dbl_j, firstAl1, firstAl, firstB411, firstB412, firstB422, d1);
+
+        //  A2 (gamma 2)
+        double a2gamma2 = calculateComponent(2, dbl_jf, dbl_j, secondAl1, secondAl, secondB211, secondB212, secondB222, d2);
+
+        //  A4 (gamma 2)
+        double a4gamma2 = calculateComponent(4, dbl_jf, dbl_j, secondAl1, secondAl, secondB411, secondB412, secondB422, d2);
+
+        a22 = a2gamma1*a2gamma2;
+        a24 = a2gamma1*a4gamma2;
+        a42 = a4gamma1*a2gamma2;
+        a44 = a4gamma1*a4gamma2;
+
+        valid = true;
+    }
+
+    double calculateComponent(int k, int jx_2, int j_2, double Al1, double Al, double Bx11, double Bx12, double Bx22, double delta)
+    {
+        int lx_2;
+        if (jx_2 >= j_2)
+            lx_2 = jx_2-j_2;
+        else
+            lx_2 = j_2-jx_2;
+
+        int lxPlusOne_2 = lx_2 + 2;
+
+        double de = sqrt(Al1/Al)*delta;
+
+        double f11 = Bx11*(((jx_2+j_2)/2+1)%2 ? -1 : 1)*sqrt((lx_2+1.)*(lx_2+1.)*(2*k+1.)*(j_2+1.))
+        *gsl_sf_coupling_3j(lx_2, lx_2, 2*k, 2, -2, 0.)
+        *gsl_sf_coupling_6j(j_2, j_2, 2*k, lx_2, lx_2, jx_2);
+
+        double f12 = Bx12*(((jx_2+j_2)+1)%2 ? -1 : 1)*sqrt((lx_2+1.)*(lxPlusOne_2+1.)*(2*k+1.)*(j_2+1.))
+        *gsl_sf_coupling_3j(lx_2, lxPlusOne_2, 2*k, 2, -2, 0.)
+        *gsl_sf_coupling_6j(j_2, j_2, 2*k, lx_2, lxPlusOne_2, jx_2);
+
+        double f22 = Bx22*(((jx_2+j_2)+1)%2 ? -1 : 1)*sqrt((lxPlusOne_2+1.)*(lxPlusOne_2+1.)*(2*k+1.)*(j_2+1.))
+        *gsl_sf_coupling_3j(lxPlusOne_2, lxPlusOne_2, 2*k, 2, -2, 0.)
+        *gsl_sf_coupling_6j(j_2, j_2, 2*k, lxPlusOne_2, lxPlusOne_2, jx_2);
+
+        return (f11-2.*de*f12+de*de*f22)/(1+de*de);
+    }
+
+};
 
 
-// Constructor is used to ser default inital values
-AkkCalc::AkkCalc()
+Akk::Akk()
+    : d(new AkkPrivate)
 {
-    ji = 0.0;
-    j = 0.0; // intermediate state spin
-    jf = 0.0; // final state spin
-    d1 = 0.0; // 
-    d2 = 0.0;
-    firstAl = 1.0;
-    firstAl1 = 1.0;
-    firstB211 = 1.0;
-    firstB212 = 1.0;
-    firstB222 = 1.0;
-    firstB411 = 1.0;
-    firstB412 = 1.0;
-    firstB422 = 1.0;
-    secondAl = 1.0;
-    secondAl1 = 1.0;
-    secondB211 = 1.0;
-    secondB212 = 1.0;
-    secondB222 = 1.0;
-    secondB411 = 1.0;
-    secondB412 = 1.0;
-    secondB422 = 1.0;
-    
-    a22 = 0.0;
-    a24 = 0.0;
-    a42 = 0.0;
-    a44 = 0.0;
 }
 
-void AkkCalc::calculateAkk()
+Akk::~Akk()
 {
-    int k;
-    double de, f11, f12, f22, l1, l2, l1PlusOne, l2PlusOne;
-    double a2gamma1, a4gamma1, a2gamma2, a4gamma2;
-    
-    
-    if ((ji-j)>=0) {
-        l1 = ji-j;
-    }
-    else {
-        l1 = j-ji;
-    }
-    
-    if ((jf-j)>=0) {
-        l2 = jf-j;
-    }
-    else {
-        l2 = j-jf;
-    }
-    
-    l1PlusOne = l1+1;
-    l2PlusOne = l2+1;
-    
-    
-    //##########
-    //  A2 (gamma 1)
-    //##########
-    
-    k = 2;
-    de = sqrt(firstAl1/firstAl)*d1;
-    
-    f11 = firstB211*pow(-1.,ji+j+1)*sqrt((2.*l1+1.)*(2.*l1+1.)*(2*k+1.)*(2*j+1.))
-        *gsl_sf_coupling_3j((int)(2.*l1),(int)(2.*l1),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-        *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l1),(int)(2.*l1),(int)(2.*ji));
-    
-    f12 = firstB212*pow(-1.,ji+j+1)*sqrt((2.*l1+1.)*(2.*l1PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l1),(int)(2.*l1PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l1),(int)(2.*l1PlusOne),(int)(2.*ji));
-    
-    f22 = firstB222*pow(-1.,ji+j+1)*sqrt((2.*l1PlusOne+1.)*(2.*l1PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l1PlusOne),(int)(2.*l1PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l1PlusOne),(int)(2.*l1PlusOne),(int)(2.*ji));
-    
-    a2gamma1 = (f11-2.*de*f12+de*de*f22)/(1+de*de);
-    
-    //##########
-    //  A4 (gamma 1)
-    //##########
-    
-    k = 4;
-    de = sqrt(firstAl1/firstAl)*d1;
-    
-    f11 = firstB411*pow(-1.,ji+j+1)*sqrt((2.*l1+1.)*(2.*l1+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l1),(int)(2.*l1),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l1),(int)(2.*l1),(int)(2.*ji));
-    
-    f12 = firstB412*pow(-1.,ji+j+1)*sqrt((2.*l1+1.)*(2.*l1PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l1),(int)(2.*l1PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l1),(int)(2.*l1PlusOne),(int)(2.*ji));
-    
-    f22 = firstB422*pow(-1.,ji+j+1)*sqrt((2.*l1PlusOne+1.)*(2.*l1PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l1PlusOne),(int)(2.*l1PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l1PlusOne),(int)(2.*l1PlusOne),(int)(2.*ji));
-    
-    a4gamma1 = (f11-2.*de*f12+de*de*f22)/(1+de*de);
-    
-    //##########
-    //  A2 (gamma 2)
-    //##########
-    
-    k = 2;
-    de = sqrt(secondAl1/secondAl)*d2;
-    
-    f11 = secondB211*pow(-1.,jf+j+1)*sqrt((2.*l2+1.)*(2.*l2+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l2),(int)(2.*l2),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l2),(int)(2.*l2),(int)(2.*jf));
-    
-    f12 = secondB212*pow(-1.,jf+j+1)*sqrt((2.*l2+1.)*(2.*l2PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l2),(int)(2.*l2PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l2),(int)(2.*l2PlusOne),(int)(2.*jf));
-    
-    f22 = secondB222*pow(-1.,jf+j+1)*sqrt((2.*l2PlusOne+1.)*(2.*l2PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l2PlusOne),(int)(2.*l2PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l2PlusOne),(int)(2.*l2PlusOne),(int)(2.*jf));
-    
-    a2gamma2 = (f11+2.*de*f12+de*de*f22)/(1+de*de);
-    
-    //##########
-    //  A4 (gamma 4)
-    //##########
-    
-    k = 4;
-    de = sqrt(secondAl1/secondAl)*d2;
-    
-    f11 = secondB411*pow(-1.,jf+j+1)*sqrt((2.*l2+1.)*(2.*l2+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l2),(int)(2.*l2),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l2),(int)(2.*l2),(int)(2.*jf));
-    
-    f12 = secondB412*pow(-1.,jf+j+1)*sqrt((2.*l2+1.)*(2.*l2PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l2),(int)(2.*l2PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l2),(int)(2.*l2PlusOne),(int)(2.*jf));
-    
-    f22 = secondB422*pow(-1.,jf+j+1)*sqrt((2.*l2PlusOne+1.)*(2.*l2PlusOne+1.)*(2*k+1.)*(2*j+1.))
-    *gsl_sf_coupling_3j((int)(2.*l2PlusOne),(int)(2.*l2PlusOne),(int)(2.*k),(int)(2.*1.),(int)(2.*(-1.)),0.)
-    *gsl_sf_coupling_6j((int)(2.*j),(int)(2.*j),(int)(2.*k),(int)(2.*l2PlusOne),(int)(2.*l2PlusOne),(int)(2.*jf));
-    
-    a4gamma2 = (f11+2.*de*f12+de*de*f22)/(1+de*de);
-            
-    a22 = a2gamma1*a2gamma2;
-    a24 = a2gamma1*a4gamma2;
-    a42 = a4gamma1*a2gamma2;
-    a44 = a4gamma1*a4gamma2;
+    delete d;
+}
+
+void Akk::setInitialStateSpin(int doubledSpin)
+{
+    d->dbl_ji = doubledSpin;
+    d->valid = false;
+}
+
+void Akk::setIntermediateStateSpin(int doubledSpin)
+{
+    d->dbl_j = doubledSpin;
+    d->valid = false;
+}
+
+void Akk::setFinalStateSpin(int doubledSpin)
+{
+    d->dbl_jf = doubledSpin;
+    d->valid = false;
+}
+
+void Akk::setPopulatingGammaMixing(double delta)
+{
+    d->d1 = delta;
+    d->valid = false;
+}
+
+void Akk::setDepopulatingGammaMixing(double delta)
+{
+    d->d2 = delta;
+    d->valid = false;
+}
+
+double Akk::a22()
+{
+    if (!d->valid)
+        d->calculateAkk();
+    return d->a22;
+}
+
+double Akk::a24()
+{
+    if (!d->valid)
+        d->calculateAkk();
+    return d->a24;
+}
+
+double Akk::a42()
+{
+    if (!d->valid)
+        d->calculateAkk();
+    return d->a42;
+}
+
+double Akk::a44()
+{
+    if (!d->valid)
+        d->calculateAkk();
+    return d->a44;
 }
